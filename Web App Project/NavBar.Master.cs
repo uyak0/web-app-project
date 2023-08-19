91ec6b70ef015d18019a56a53fc30ebbc4edbc97
uyak0 <milkycoffeethings@gmail.com>
2023-08-19 14:47:24 +0800

    +master page (template w/ navbar)

--- Web App Project/NavBar.Master.cs
+++ Web App Project/NavBar.Master.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace Web_App_Project
 {
-    public partial class WebForm2 : System.Web.UI.Page
+    public partial class Site1 : System.Web.UI.MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
