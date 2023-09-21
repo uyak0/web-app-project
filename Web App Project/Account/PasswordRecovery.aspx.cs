638011218cbde2d4b606385a1f46641fa25009b2
uyak0 <milkycoffeethings@gmail.com>
2023-09-22 00:09:58 +0800

    +passwordrecovery

--- Web App Project/Account/PasswordRecovery.aspx.cs
+++ Web App Project/Account/PasswordRecovery.aspx.cs
@@ -1,18 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data.SqlClient;
 using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
 using System.Web;
-using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
 namespace Web_App_Project.Account
 {
-    public partial class SignUp : System.Web.UI.Page
+    public partial class PasswordRecovery : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
